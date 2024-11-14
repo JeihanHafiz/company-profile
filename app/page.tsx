51986e9792abad2f3a9b7ed3067a1580a8323356
unknown <jeihanhafiz31595@gmail.com>
2024-11-14 23:14:25 +0700

    v.3

--- app/page.tsx
+++ app/page.tsx
@@ -28,64 +28,36 @@ export default async function Home() {
         </video>
         <div className={styles.img_hero}></div>
         <h1>We Are Elan</h1>
-        <h3>Dedicated to protect ground forces for over than 2 decades.</h3>
+        <h2>Dedicated to protect ground forces for over than 2 decades.</h2>
       </section>
 
       {/* profile */}
       <section className={styles.our_info}>
         <div className={styles.info}>
-          <h1>Our Company</h1>
+          <h1>Elan, where only the passionate and aggresive wins the day.</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
-            exercitationem eveniet mollitia aperiam suscipit quae quidem, minus
-            minima rem animi molestiae debitis impedit veritatis et, ex ipsam?
-            Temporibus, porro saepe quaerat veniam, quia exercitationem harum,
-            aperiam consequatur odio enim fuga praesentium. Error quas dolorum
-            repudiandae, reprehenderit molestiae quis, labore nemo eum minima
-            repellat ut sit. Voluptatem hic officiis distinctio nulla pariatur
-            quasi atque voluptatum qui quae porro id, rerum corporis vitae nemo
-            sit totam praesentium accusantium ipsum. Explicabo nesciunt culpa,
-            expedita cupiditate illum ex esse! Non similique optio eos neque ad
-            omnis voluptate modi est eum. Aut quasi rerum fugit!
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore
-            beatae similique sit quasi quos. Asperiores earum provident
-            architecto magni, id reiciendis dolore aperiam numquam in aut error
-            non laborum expedita placeat laudantium minus cupiditate vel
-            pariatur quo accusamus? Amet molestiae ratione aliquid consequuntur
-            cumque excepturi nemo veritatis iste consequatur. Voluptatum aliquam
-            voluptatem, veniam nihil provident commodi ab nam aliquid pariatur
-            quidem mollitia quas odit totam illo esse omnis. Consequatur odio
-            libero, accusantium recusandae fuga ipsa iusto cumque laborum minima
-            odit expedita sed fugiat quam tenetur, ex adipisci nisi, in fugit
-            iste dignissimos minus sequi. Optio, quos. Quaerat blanditiis
-            laborum fugiat.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
-            magnam reprehenderit nobis ea nam cum soluta itaque id animi, quos
-            tempore voluptas quod numquam voluptate architecto deserunt
-            temporibus reiciendis corporis sed ipsa voluptatibus. Praesentium,
-            molestias velit possimus voluptate tenetur eveniet molestiae
-            deserunt! Sunt corporis eos exercitationem voluptatem illum ducimus
-            obcaecati amet dolorum. Sequi provident assumenda nam molestiae
-            cupiditate! Inventore dignissimos non sapiente. Repudiandae repellat
-            odio accusantium labore illum molestias, iste neque, quo officiis
-            autem impedit quisquam nihil velit molestiae dolorem vero porro
-            provident et quaerat iusto. Voluptates corrupti itaque vel
-            exercitationem sint sequi, ut autem ea ipsa ex doloremque ipsum?
+            Born at post-pandemic year, Elan answer the solution to further improve national defense capabilities, by producing reliable yet durable firearms and match-grade ammunition where ground-defense are our specialty and will never changes.
           </p>
         </div>
       </section>
 
-      {/* catalogs */}
-      {/* <section className={styles.catalog_section}>
-        <div className={styles.catalog_container}>
-          <h1>Our Catalogs</h1>
-          
+      <section className={styles.review_section}>
+        <h1>Our Clients</h1>
+        <div className={styles.review_box}>
+          <div className={styles.review}>
+            <h1>Hristo Todorov</h1>
+            <p>Elan&apos;s 7.62mm Sub-sonic delivered good performance, but most important of all; the sound is nearly unheard at 30m where none other could!</p>
+          </div>
+          <div className={styles.review}>
+            <h1>Gen. Markus Dubka</h1>
+            <p>Their FA-MAS(FA Mod. T1) are delivering desirable performance, and what surprised me a lot it how they are very ambidextrous-friendly.</p>
+          </div>
+          <div className={styles.review}>
+            <h1>Joseph Stiller</h1>
+            <p>We decided to procure their 25mm and going to test-fire from our KBA mounted in Valhalla, and surprisingly the velocity is very fast while being precise on 100th shots. Can be useful against oncoming</p>
+          </div>
         </div>
-      </section> */}
+      </section>
 
       {/* press release */}
       <section className={styles.post_section}>
